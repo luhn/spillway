366dc4f5b238a8c444d1975a254e496ff5068cf3
Theron Luhn <theron@luhn.com>
2020-02-04 16:06:36 -0800

    Update sample app to allow pausing.

--- sampleapp/app.py
+++ sampleapp/app.py
@@ -1,5 +1,14 @@
+from time import sleep
+
 
 def application(environ, start_response):
+    sleep_str = environ['PATH_INFO'].strip('/')
+    if sleep_str:
+        try:
+            sleep_float = float(sleep_str)
+        except ValueError:
+            raise ValueError('Path must be an integer or float.')
+        sleep(sleep_float)
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response(status, headers)
